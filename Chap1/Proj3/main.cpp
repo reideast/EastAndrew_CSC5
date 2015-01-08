c244a849749a67caf98cb9cc1baa0a2701b06efc
Andrew East <aeast@student.rcc.edu>
2015-01-08 13:52:03 -0800

    Signed-off-by: Andrew East <aeast@student.rcc.edu>

---
+++ Chap1/Proj3/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew East
+ * Purpose: To add and multiply two integers
+ *
+ * Created on January 8, 2015, 1:48 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int quarters = 0, dimes = 0, nickels = 0;
+    float value = 0.0f;
+    
+    cout << "How much change is in your pocket?"
+    
+    cout << "Please enter two whole numbers, and the press enter: ";
+    cin >> a;
+    
+    sum = a + b;
+    product = a * b;
+    
+    cout << "The sum of "  << a << " and " << b << " is " << sum << endl;
+    cout << "The product of " << a << " and " << b << " is " << product << endl;
+    
+    return 0;
+}
+
