43992d63558a6daa62b2a3924a4bfd1dde4ea405
Andrew East <aeast@student.rcc.edu>
2015-01-08 19:16:55 -0800

    Signed-off-by: Andrew East <aeast@student.rcc.edu>

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* 
+   File:   main.cpp
+   Author: Andrew Reid East
+	 Class: CSC-5 40718
+   Created on January 6, 2015, 6:05 PM
+   Purpose: template
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+//Global Constants
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv)
+{
+  cout << "Hello, world!" << endl;
+  
+  return 0;
+}
