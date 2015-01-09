7fbe55a6a8d2980c2cbf685063ea442e8378fb51
Andrew East <aeast@student.rcc.edu>
2015-01-08 22:39:58 -0800

    Change HelloWorld to match homework assignment. Signed-off-by: Andrew East <aeast@student.rcc.edu>

--- Class_Lab/HelloWorld/main.cpp
+++ Class_Lab/HelloWorld/main.cpp
@@ -3,8 +3,6 @@
    Author: Andrew Reid East
    Created on January 6, 2015, 6:05 PM
    Purpose: Our first program
-   I am up at github now!
-   Testing repository sync again!
  */
 
 //System Libraries
@@ -23,22 +21,8 @@ int main(int argc, char** argv)
     //Output Hello World
     cout << "Hello world!" << endl << endl;
     
-    bool isProgramError = false;
-    int userChoice = 0;
-    
-    cout << "Do you want the program to work? (1 for yes, 0 for no (or any string to mess things up, uhh)) ";
-    cin >> userChoice;
-    
-    if (userChoice == 1)
-        isProgramError = false;
-    else if (userChoice == 0)
-        isProgramError = true;
-    else
-    {
-        cout << "Your input was not valid." << endl << "I'm going to fail the program out of spite." << endl;
-        isProgramError = true;
-    }
-    
-    return isProgramError;
+    return 0;
 }
 
+// I am up at github now!
+//Testing repository sync again!
