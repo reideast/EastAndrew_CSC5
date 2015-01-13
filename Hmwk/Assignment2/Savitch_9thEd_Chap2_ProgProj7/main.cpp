67323c8425ac8e47c16d17f118223d80b87831c3
Andrew East <aeast@student.rcc.edu>
2015-01-12 22:27:17 -0800

    Signed-off-by: Andrew East <aeast@student.rcc.edu>

--- Hmwk/Assignment2/Savitch_9thEd_Chap2_ProgProj7/main.cpp
+++ Hmwk/Assignment2/Savitch_9thEd_Chap2_ProgProj7/main.cpp
@@ -21,6 +21,31 @@ int main(int argc, char** argv)
   //set output formatting
   cout << fixed << setprecision(2) << showpoint;
   
+  float currentCost = 0.0f, inflation = 0.0f;
+  int targetTime = 0;
+  
+  cout << "What is the current cost? $";
+  cin >> currentCost;
+  cout << "How many years from now would you like to be calculated? ";
+  cin >> targetTime;
+  cout << "What is the estimated inflation rate as a percentage? ";
+  cin >> inflation;
+  cout << endl;
+  
+  inflation /= 100.0f; //convert whole number percentage to a fraction
+  //cout << "DEBUG: inflation: " << inflation << endl;
+  
+  int countTime = 0;
+  float inflatedCost = currentCost;
+  //cout << "DEBUG: inflatedCost: " << inflatedCost << endl;
+  while (countTime < targetTime) //loop once for each year
+  {
+    inflatedCost += inflatedCost * inflation; //add the percentage increased
+    ++countTime;
+    //cout << "DEBUG: " << countTime<< "/inflatedCost: " << inflatedCost << endl;
+  }
+  
+  cout << "The cost after " << countTime << " years at " << inflation * 100.0f << "% inflation  will be $" << inflatedCost << endl;
   
   return 0;
 }
