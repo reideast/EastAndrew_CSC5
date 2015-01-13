3338d09adf9b7c3eb48947909d2a337adde49a89
Andrew East <aeast@student.rcc.edu>
2015-01-12 21:25:55 -0800

    Signed-off-by: Andrew East <aeast@student.rcc.edu>

---
+++ Hmwk/Assignment2/Savitch_9thEd_Chap2_ProgProj7/main.cpp
@@ -0,0 +1,26 @@
+/* 
+    File:   main.cpp
+    Author: Andrew Reid East
+    Class: CSC-5 40718
+    Created on January 12, 2015, 9:22 PM
+    Purpose: Calculate cost due to inflation
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//User Libraries
+//Global Constants
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv)
+{
+  //set output formatting
+  cout << fixed << setprecision(2) << showpoint;
+  
+  
+  return 0;
+}
