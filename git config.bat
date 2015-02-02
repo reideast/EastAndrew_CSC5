9e783c0e1050c3f6c6aaf3bd8b5fe293526ab093
Andrew East <aeast@student.rcc.edu>
2015-02-01 21:54:35 -0800

    Signed-off-by: Andrew East <aeast@student.rcc.edu>

--- git config.bat
+++
@@ -1,7 +0,0 @@
-git config --file "config_user_only"
-rem git config --global user.email "aeast@student.rcc.edu"
-rem git config --global user.name "Andrew East"
-
-start "" "C:\Program Files (x86)\Git\bin\wish.exe" "C:\Program Files (x86)\Git\libexec\git-core\git-gui"
-
-rem pause
