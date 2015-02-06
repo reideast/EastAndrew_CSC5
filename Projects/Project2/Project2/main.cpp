ca771460a5a68cee6f9e1b856132f8757cc380c8
Andrew East <aeast@student.rcc.edu>
2015-02-05 17:57:18 -0800

    Signed-off-by: Andrew East <aeast@student.rcc.edu>

---
+++ Projects/Project2/Project2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on February 5, 2015, 5:56 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
